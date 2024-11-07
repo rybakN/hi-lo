72b221904fbde0bdb9bd7fd55efbe5558d21119a
Mikalai Davydau <101674084+rybakN@users.noreply.github.com>
2024-11-07 13:17:36 +0300

    feat: added a project description

--- src/interfaces/player.interface.ts
+++ src/interfaces/player.interface.ts
@@ -1,8 +1,6 @@
 import { ParseBetType } from "../types/parseBet.type";
 
 export interface PlayerInterface {
-  balance: number;
-  name: string;
   makeBet(bet: ParseBetType): number | null;
   takeMoney(sum: number): void;
 }
